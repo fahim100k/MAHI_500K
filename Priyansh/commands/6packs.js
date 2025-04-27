1eda5ec06619000da2cc0574ec045f080ef9ece5
fahim100k <fahimaislam09173@gmail.com>
2025-04-27 21:05:41 +0600

    Update 6packs.js

--- Priyansh/commands/6packs.js
+++ Priyansh/commands/6packs.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "6mui",
+  name: "dp",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
@@ -20,15 +20,15 @@ module.exports.run = async({api, event, args, Users, Threads, Currencies}) => {
   const fs = global.nodemodule["fs-extra"];
   var link = [
     "https://i.imgur.com/abcd123.jpg",  // Replace with actual URLs of Bangladeshi cute girls
-    "https://i.imgur.com/efgh456.jpg",
-    "https://i.imgur.com/ijkl789.jpg",
-    "https://i.imgur.com/mnop012.jpg",
-    "https://i.imgur.com/qrst345.jpg",
-    "https://i.imgur.com/uvwx678.jpg",
-    "https://i.imgur.com/yzaa901.jpg",
-    "https://i.imgur.com/bbcd234.jpg",
-    "https://i.imgur.com/defg567.jpg",
-    "https://i.imgur.com/ghij890.jpg"
+    "https://i.imgur.com/IXbmFeC.jpeg",
+    "https://i.imgur.com/N4WIYHj.jpeg",
+    "https://i.imgur.com/3hRlpV3.jpeg",
+    "https://i.imgur.com/pG8Co9M.jpeg",
+    "https://i.imgur.com/mhCOXbq.jpeg",
+    "https://i.imgur.com/3qv0nwM.jpeg",
+    "https://i.imgur.com/62hAtfz.jpeg",
+    "https://i.imgur.com/eL3vjXE.jpeg",
+    "https://i.imgur.com/ijihf8s.jpeg"
   ];
 
   var callback = () => api.sendMessage({body: `এটা তো কিউট বাংলাদেশের মেয়ে! দেখো!`, attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
